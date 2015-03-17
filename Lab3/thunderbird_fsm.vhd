93c0629d2d8a8272e1ea4f7c989a976b0d3c16be
Dominic Celiano <C17Dominic.Celiano@usafa.edu>
2015-03-17 17:22:09 -0600

    Committed finished thunderbird_fsm.vhd file - all the logic was added for the FSM.

--- Lab3/thunderbird_fsm.vhd
+++ Lab3/thunderbird_fsm.vhd
@@ -30,7 +30,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity thunderbird_fsm is
     Port ( clk, reset, left, right : in  STD_LOGIC;
-           lights_l, lights_r : out  STD_LOGIC_VECTOR (2 downto 0));
+           lights_l, lights_r : out  STD_LOGIC_VECTOR (2 downto 0)); --0 is left-hand light in bus, 2 is right-hand light
 end thunderbird_fsm;
 
 architecture Behavioral of thunderbird_fsm is
@@ -48,22 +48,46 @@ begin
 	--next state logic
 	process(left, right) begin
 		case state is
-			when S0 => if a = '1' then nextstate <= S1;
+			when S0 => if (left = '1' and right = '0' and reset = '0') 
+								then nextstate <= S1;
+						  elsif (left = '0' and right = '1' and reset = '0')
+								then nextstate <= S4;
+						  elsif (left = '1' and right = '1' and reset = '0')
+								then nextstate <= S7;
 						  else nextstate <= S0;
 						  end if;
-			when S1 => if b = '1' then nextstate <= S2;
+			when S1 => if reset = '0' then nextstate <= S2;
 						  else nextstate <= S0;
 						  end if;
-			when S2 => if (a = '1' and b = '1') then nextstate <= S2;
+			when S2 => if reset = '0' then nextstate <= S3;
 						  else nextstate <= S0;
 						  end if;
+			when S3 => nextstate <= S0;
+			when S4 => if reset = '0' then nextstate <= S5;
+						  else nextstate <= S0;
+						  end if;
+			when S5 => if reset = '0' then nextstate <= S6;
+						  else nextstate <= S0;
+						  end if;
+			when S6 => nextstate <= S0;
+			when S7 => nextstate <= S2;
 			when others => nextstate <= S0;
 		end case;
 	end process;
 	
 	--output logic
-	--y <= '1' when(state = S2 and a = '1' and b = '1')
-		--		else '0';
+	lights_l(0) <= '1' when(state = S3 or state = S7) -- LC (left-most light) = S3 + S7
+				else '0';
+	lights_l(1) <= '1' when(state = S2 or state = S3 or state = S7) -- LB = S2 + S3+ S7
+				else '0';
+	lights_l(2) <= '1' when(state = S1 or state = S2 or state = S3 or state = S7) -- LA = S1 + S2+ S3 + S7
+				else '0';
+	lights_r(0) <= '1' when(state = S4 or state = S5 or state = S6 or state = S7) -- RA = S4 + S5 + S6 + S7
+				else '0';
+	lights_r(1) <= '1' when(state = S5 or state = S6 or state = S7) -- RB = S5 + S6 + S7
+				else '0';
+	lights_r(2) <= '1' when(state = S6 or state = S7) -- RC (right-most light) = S6 + S7
+				else '0';
 
 end Behavioral;
 
